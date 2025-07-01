976a1ed02176b14a8efa16b0d644293dbdb2606e
aufardp <aufardanu@gmail.com>
2025-07-01 13:42:11 +0700

    feat: add lanyard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
+import Lanyard from "./components/Lanyard/Lanyard";
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <div className="container border mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">A</div>
+          <div className="col-span-6">
+            <Lanyard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
